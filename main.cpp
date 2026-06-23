75ba04bc58eb59855cc02f21fa1b65aaa2c2c83d
HELLORÁ BUENO MIRANDA <hellora_miranda@estudante.sesisenai.org.br>
2026-06-23 16:22:56 -0300

    modificada a saudaçao

--- main.cpp
+++ main.cpp
@@ -3,11 +3,9 @@
 using namespace std;
 
 void saudacao() {
-<<<<<<< HEAD
+
 	cout << "É us guri do senai tmj" << endl;
-=======
-	cout << "Seja mais ou menos vindo!" << endl;
->>>>>>> 3be9d4b0fd47776bdee4a80847514bec1226b451
+
 }
 
 int main () {
